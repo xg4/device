932d7d39cbb477849113b6f916ac43afad096db9
xg4 <xingor4@gmail.com>
2019-06-02 13:24:25 +0800

    test: add tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,68 @@
 import Device from '../src'
+import UA from './user-agent'
 
 test('Successfully created an instance', () => {
   expect(new Device()).toBeInstanceOf(Device)
+})
+
+test('mac os', () => {
+  const device = new Device(UA.mac)
+  expect(device.isMac()).toBeTruthy()
+  expect(device.isIos()).toBeFalsy()
+  expect(device.isDesktop()).toBeTruthy()
+})
+
+test('android phone', () => {
+  const device = new Device(UA.androidPhone)
+  expect(device.isAndroidPhone()).toBeTruthy()
+  expect(device.isMobile()).toBeTruthy()
+})
+
+test('android phone galaxy s5', () => {
+  const device = new Device(UA.galaxyS5)
+  expect(device.isAndroidPhone()).toBeTruthy()
+  expect(device.isMobile()).toBeTruthy()
+})
+
+test('android phone pixel 2', () => {
+  const device = new Device(UA.pixel2)
+  expect(device.isAndroidPhone()).toBeTruthy()
+  expect(device.isMobile()).toBeTruthy()
+})
+
+test('blackberry phone', () => {
+  const device = new Device(UA.blackberryPhone)
+  expect(device.isBlackberryPhone()).toBeTruthy()
+  expect(device.isMobile()).toBeTruthy()
+})
+
+test('iPhone ios', () => {
+  const device = new Device(UA.iPhone)
+  expect(device.isIos()).toBeTruthy()
+  expect(device.isIphone()).toBeTruthy()
+  expect(device.isMobile()).toBeTruthy()
+})
+
+test('iPad ios', () => {
+  const device = new Device(UA.iPad)
+  expect(device.isIos()).toBeTruthy()
+  expect(device.isIpad()).toBeTruthy()
+  expect(device.isTablet()).toBeTruthy()
+})
+
+test('blackberry Z30', () => {
+  const device = new Device(UA.blackberryZ30)
+  expect(device.isBlackberryPhone()).toBeTruthy()
+  expect(device.isMobile()).toBeTruthy()
+})
+
+test('windows phone', () => {
+  const device = new Device(UA.lumia950)
+  expect(device.isWindowsPhone()).toBeTruthy()
+  expect(device.isMobile()).toBeTruthy()
+})
+
+test('node', () => {
+  const device = new Device()
+  expect(device.isNode()).toBeTruthy()
 })
---
+++ test/user-agent.ts
@@ -0,0 +1,21 @@
+export default {
+  mac:
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+  androidPhone:
+    'Mozilla/5.0 (Linux; U; Android 2.3.5; zh-cn; HTC_IncredibleS_S710e Build/GRJ90) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+  blackberryPhone:
+    'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.1.0.346 Mobile Safari/534.11+',
+  // chrome mock
+  iPhone:
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+  iPad:
+    'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1',
+  galaxyS5:
+    'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Mobile Safari/537.36',
+  pixel2:
+    ' Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Mobile Safari/537.36',
+  blackberryZ30:
+    'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.10+ (KHTML, like Gecko) Version/10.0.9.2372 Mobile Safari/537.10+',
+  lumia950:
+    'Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 950) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Mobile Safari/537.36 Edge/14.14263'
+}
